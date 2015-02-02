e9ca63087b966df239f0909cded3817c7ea44330
Derek Szydlowski <ramen74@gmail.com>
2015-02-02 17:31:10 +0900

    Fix syntax in arg

--- goship.go
+++ goship.go
@@ -39,8 +39,8 @@ var (
 	sshPort           = "22"
 	keyPath           = flag.String("k", "id_rsa", "Path to private SSH key (default id_rsa)")
 	dataPath          = flag.String("d", "data/", "Path to data directory (default ./data/)")
-	ETCDServer        = flag.String("e", "http://127.0.0.1:4001", "Etcd Server (default http://127.0.0.1:4001")
-	cookieSessionHash = flag.String("c", "jhjhjhjhjhjjhjhhj", "Random cookie session key (default jhjhjhjhjhjjhjhhj")
+	ETCDServer        = flag.String("e", "http://127.0.0.1:4001", "Etcd Server (default http://127.0.0.1:4001)")
+	cookieSessionHash = flag.String("c", "jhjhjhjhjhjjhjhhj", "Random cookie session key (default jhjhjhjhjhjjhjhhj)")
 )
 
 var store = sessions.NewCookieStore([]byte(*cookieSessionHash))
