683c25a6684ddff61e22ddc733464c708380b068
Kelvin Tay <kelvintaywl@gmail.com>
2015-05-15 10:34:10 +0900

    try removing variables in index template

--- goship.go
+++ goship.go
@@ -742,7 +742,7 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 	js, css := getAssetsTemplates()
-	t.ExecuteTemplate(w, "base", map[string]interface{}{"Javascript": js, "Stylesheet": css, "Projects": c.Projects, "User": u, "Page": "home", "ConfirmDeployFlag": *confirmDeployFlag, "GithubToken": os.Getenv(gitHubAPITokenEnvVar), "PivotalToken": c.Pivotal.Token})
+	t.ExecuteTemplate(w, "base", map[string]interface{}{"Javascript": js, "Stylesheet": css, "Projects": c.Projects, "User": u, "Page": "home", "ConfirmDeployFlag": *confirmDeployFlag})
 }
 
 func getAssetsTemplates() (js, css template.HTML) {
